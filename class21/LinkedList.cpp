d08f90a5075eb1a0369126343fa948e2be2d8811
Ashraya Kadel <ashrayakadel28@gmail.com>
2024-04-10 05:59:57 +0100

    Second commit

--- class21/LinkedList.cpp
+++ class21/LinkedList.cpp
@@ -1,1 +1,31 @@
-class Mode {};
+class Node {
+    private:
+
+    public:
+        int info;
+        Node* next;
+        Node(int infoin, Node* next) {
+            info = infoin;
+            next=nullptr;
+        }
+
+};
+
+class LinkedList {
+    private:
+        Node* HEAD;
+        Node* TAIL;
+    public:
+        LinkedList (Node){}
+        bool isEmpty();
+        void addtoHead(int data);
+        void addtoTail(int data);
+        void add(int data, Node* pred);
+        void removefromHead();
+        void removefromTail();
+        void remove(int data);
+        bool search(int data);
+        bool retrieve(int data, Node* dataOutPtr);
+        void traverse();
+        
+};
